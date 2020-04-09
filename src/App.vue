fe01abdb220d251257d112cc19e3794807ea3967
Drama Queen <dramaqueen@macintoshs-MacBook-Air.local>
2020-04-09 13:35:29 +0100

    my first blog

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+ <addBlog></addBlog>
+</div>
+</template>
+
+<script>
+
+import addBlog from './components/addBlog';
+
+export default {
+  components:{
+    addBlog:addBlog
+  },
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
+
+<style>
+body{
+  margin: 0;
+  font-family: 'Nunito SemiBold';
+}
+</style>
